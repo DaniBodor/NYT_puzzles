fc0cbce8083b6a1c9f8e412db455da6ae9ffb311
DaniBodor <d.bodor@esciencecenter.nl>
2025-09-01 15:20:20 +0200

    code

---
+++ string_concatenations.py
@@ -0,0 +1,10 @@
+explicit_multiline = (
+    "The quick brown fox jumps over the lazy "
+    + "dog"
+)
+explicit_singleline = "The quick brown fox jumps over the lazy " + "dog"
+implicit_multiline = (
+    "The quick brown fox jumps over the lazy "
+    "dog"
+)
+implicit_singleline = "The quick brown fox jumps over the lazy " "dog"
